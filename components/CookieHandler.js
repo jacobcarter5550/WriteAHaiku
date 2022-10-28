f77abc7d23b88a4f1099d045ca0fbbd55e1b5d53
jacobcarter5550 <68360718+jacobcarter5550@users.noreply.github.com>
2022-10-28 04:22:56 -0700

    Update CookieHandler.js

--- components/CookieHandler.js
+++ components/CookieHandler.js
@@ -1,5 +1,4 @@
-import {useState} from 'react'
-import { useCookies, withCookies } from 'react-cookie';
+import {  withCookies } from 'react-cookie';
 import styles from '../styles/sass/Welcome.module.scss'
 import Image from 'next/image';
 
@@ -12,8 +11,8 @@ function CookieHandler({set, state, setCook}) {
                     <div className={styles.modal}>
                         <Image src="/wh.svg" alt="" />
                         <h1>Welome to WriteHaiku!</h1>
-                        <p>WriteHaiku is easy! Write a line of your Haiku, end it with a comma, and WH will bring you generative art based on your story. You can write as many lines as you want, but once you place add a comma, the line can't be re-written.</p>
-                        <p>Once you're ready, just start typing!</p>
+                        <p>WriteHaiku is easy! Write a line of your Haiku, end it with a comma, and WH will bring you generative art based on your story. You can write as many lines as you want, but once you place add a comma, the line can&apos;t be re-written.</p>
+                        <p>Once you&apos;re ready, just start typing!</p>
                         <br />
                         <p>PS: try getting a perfect 5-7-5 haiku 👀 🎉</p>
                         <button onClick={()=>{setCook('visited', true), set(true)}}>Get Started</button>
