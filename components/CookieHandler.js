1a585ae560388a06dcf4da9990d952e9084635fe
jacobcarter5550 <68360718+jacobcarter5550@users.noreply.github.com>
2022-10-28 04:18:54 -0700

    Update CookieHandler.js

--- components/CookieHandler.js
+++ components/CookieHandler.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import { useCookies, withCookies } from 'react-cookie';
 import styles from '../styles/sass/Welcome.module.scss'
+import Image from 'next/image';
 
 function CookieHandler({set, state, setCook}) {
 
@@ -9,7 +10,7 @@ function CookieHandler({set, state, setCook}) {
             state == false ?
                 <div className={styles.welcome}>
                     <div className={styles.modal}>
-                        <img src="/wh.svg" alt="" />
+                        <Image src="/wh.svg" alt="" />
                         <h1>Welome to WriteHaiku!</h1>
                         <p>WriteHaiku is easy! Write a line of your Haiku, end it with a comma, and WH will bring you generative art based on your story. You can write as many lines as you want, but once you place add a comma, the line can't be re-written.</p>
                         <p>Once you're ready, just start typing!</p>
