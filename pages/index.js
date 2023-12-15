6382d9552a3cddd7c3d6ca2c108df5fdc639b0b9
jacobcarter5550 <68360718+jacobcarter5550@users.noreply.github.com>
2023-12-15 14:45:36 -0800

    open Pr for formating

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Home from '../components/Home'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Home from "../components/Home";
 
-export default function Index({set}) {
+export default function Index({ set }) {
   return (
-    <> 
-      <Home set={set}/>
+    <>
+      <p>broken yo</p>
+      <Home set={set} />
     </>
-  )
+  );
 }
